87d063fef1717ed68f7b430969bf8e2f9aa60710
Kelvyn <dev.kelvynjohnson@gmail.com>
2024-07-24 17:22:57 -0300

    Testa métodos iniciarLocacao e finalizarLocacao
    Testando metodos da classe service e imprime ID do recibo

--- src/test/java/br/com/fiap/tc/sistema/parquimetro/api/ReciboControllerTest.java
+++ src/test/java/br/com/fiap/tc/sistema/parquimetro/api/ReciboControllerTest.java
@@ -10,6 +10,7 @@ import br.com.fiap.tc.sistema.parquimetro.api.model.enums.StatusReciboEnum;
 import br.com.fiap.tc.sistema.parquimetro.api.model.enums.TipoPeriodoEnum;
 import br.com.fiap.tc.sistema.parquimetro.api.service.ReciboService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,13 +18,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDateTime;
-
-import static br.com.fiap.tc.sistema.parquimetro.api.model.enums.StatusPagamentoEnum.CONFIRMADO;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ReciboController.class)
@@ -35,27 +34,34 @@ public class ReciboControllerTest {
     @MockBean
     private ReciboService reciboService;
 
-    @Test
-    public void iniciarLocacaoTest() throws Exception {
-        // Configuração
-        LocacaoRequest locacaoRequest = new LocacaoRequest("123",
-                "ABC-1234",
-                new EnderecoDTO("Rua A", "123", "São Paulo", "SP", "12345-123"),
-                new Periodo(TipoPeriodoEnum.FIXO, 1));
-        ReciboDTO reciboDTO = new ReciboDTO("123",StatusReciboEnum.ABERTO, null, "1 hora", 10.0, FormaPagamentoEnum.CARTAO_CREDITO, 10.0);
+    private static String reciboId;
 
+    @Test
+    @Order(1)
+    public void iniciarLocacaoTest() throws Exception {
+        LocacaoRequest locacaoRequest = new LocacaoRequest("123", "ABC-1234", new EnderecoDTO("Rua A", "123", "São Paulo", "SP", "12345-123"), new Periodo(TipoPeriodoEnum.FIXO, 1));
+        ReciboDTO reciboDTO = new ReciboDTO("255", StatusReciboEnum.ABERTO, null, "1 hora", 10.0, FormaPagamentoEnum.CARTAO_CREDITO, 10.0);
 
         ObjectMapper objectMapper = new ObjectMapper();
         String locacaoRequestJson = objectMapper.writeValueAsString(locacaoRequest);
-        String reciboDTOJson = objectMapper.writeValueAsString(reciboDTO);
 
         given(reciboService.iniciarLocacao(any(LocacaoRequest.class))).willReturn(reciboDTO);
 
-        // Ação e Verificação
         mockMvc.perform(post("/recibos/iniciar-locacao")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(locacaoRequestJson))
-                .andExpect(status().isCreated()) // Alterado para verificar o status 201
-                .andExpect(content().json(reciboDTOJson));
+                .andExpect(status().isCreated());
+
+        reciboId = reciboDTO.id(); // Armazenar o ID do recibo para uso no próximo teste
+    }
+    @Test
+    public void finalizarLocacaoTest() throws Exception {
+        mockMvc.perform(put("/recibos/finalizar-locacao/" + reciboId) // Use a variável reciboId definida anteriormente
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        // Verificar se o método finalizarLocacao do reciboService foi chamado com o ID correto
+        verify(reciboService).finalizarLocacao(reciboId);
+        System.out.println("Recibo finalizado com sucesso!" + reciboId);
     }
 }
